1d29f8006f9007d94e8ec7f4f85a97a2ef9a85fb
JuanCardona <jcardonabageth@gmail.com>
2023-11-18 13:20:15 -0500

    app

--- src/app.js
+++ src/app.js
@@ -33,6 +33,7 @@ app.use(express.urlencoded({ limit: '1mb', extended: true }))
 
 // Routes
 app.use('/api/v1', authRoutes)
+app.use('/api/v1/hi', () => "hellow world!")
 
 // Rutas no encontradas
 app.use('*', (_req, _res, next) => {
